11e8d0a6950d818e32e1a7cdedc5bc2e16a11c1b
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-02-23 09:46:20 +0100

    fixed app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import imgRickMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
+
+  const [characters, setCharacters] = useState(null);
 
   const reqApi = async () => {
     const api = await fetch('https://rickandmortyapi.com/api/character');
     const characterApi = await api.json();
-    console.log(characterApi);
+
+    setCharacters(characterApi.results);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img 
-          src={ imgRickMorty } 
-          alt="Imagen Rick & Morty" 
-          title="Imagen Rick & Morty"
-          className="img-home" />
-          <button onClick={ reqApi } className="btn-search">Buscar Personajes</button>
+        { characters ?  (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img
+              src={imgRickMorty}
+              alt="Imagen Rick & Morty"
+              title="Rick & Morty"
+              className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
       </header>
     </div>
   );
